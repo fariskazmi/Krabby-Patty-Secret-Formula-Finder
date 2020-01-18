3cc58697c1f0592b0da2e29e2fe7e46c1c3da34a
matzit20 <54906052+matzit20@users.noreply.github.com>
2020-01-18 16:16:00 -0700

    Update tensorflowModel.py

--- tensorflowModel.py
+++ tensorflowModel.py
@@ -1,19 +1,20 @@
+print("hi")
 import sys
-sys.path.append(r"C:\Users\vince\AppData\Local\Programs\Python\Python37\Lib\site-packages")
+sys.path.append(r"C:\Users\matthewzita\AppData\Local\Programs\Python\Python37\Lib\site-packages")
 
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#import urllib3
+#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 print("yuh")
 
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
-tfds.disable_progress_bar()
+
 
 print("aiyaaaa")
 
-from tensorflow.keras import layers
+
 
 splits = tfds.Split.ALL.subsplit(weighted=(75,25))
 splits, info = tfds.load('food101', with_info=True, split=splits)
