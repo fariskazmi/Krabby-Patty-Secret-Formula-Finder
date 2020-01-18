e807b13cfb4a8c1f9833d60a4feaf7878b063b6e
matzit20 <54906052+matzit20@users.noreply.github.com>
2020-01-18 14:53:19 -0700

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -8,13 +8,12 @@ response = requests.get('https://www.foodnetwork.com/recipes/ellie-krieger/break
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # find all elements beginning with the following
-ingredients = []
-ingredients.append(soup.find_all(class_='o-Ingredients__a-Ingredient').get_text())
+ingred_h = soup.find_all(class_='o-Ingredients__a-Ingredient')
 
 # get list of ingredients as a string
-#ingredients = []
-#for i in range(len(ingred_h)):
-#    ingredients.append(str(ingred_h[i])[39:-4])
+ingredients = []
+for i in range(len(ingred_h)):
+    ingredients.append(str(ingred_h[i].get_text()))
 
 for ingredient in ingredients:
     print(ingredient)
